8ae86d7ab635cc40889647de25baee5fbeefbb44
Yash Kumar Rajput <yash.komal24@gmail.com>
2023-05-28 00:00:42 +0530

    file read impl

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+    let contents = fs::read_to_string(filename).expect("Error reading the file");
+    println!("{:?}", contents);
+}
