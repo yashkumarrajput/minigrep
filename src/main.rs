19cf1a0f1f8f5355b6430e9460e347c3d9362251
Yash Kumar Rajput <yash.komal24@gmail.com>
2023-05-28 21:51:22 +0530

    added case insensitive flag

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,19 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str>{
-        if args.len() < 3 {
-            return Err("Not Enough Arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config {query, filename})
-    }
-}
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|error| {
-        println!("Problem parsing Arguments: {}", error);
+        eprintln!("Problem parsing Arguments: {}", error);
         process::exit(1);
     });
 
-    if let Err(e) = run(config) {
-        println!("Application Error: {}", e);
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application Error: {}", e);
         process::exit(1);
     }
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-    println!("{:?}", contents);
-    Ok(())
-}
+
