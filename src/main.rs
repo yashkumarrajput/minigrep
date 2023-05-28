e048bc1e59ec4072217ddc2351c05d475fc10db9
Yash Kumar Rajput <yash.komal24@gmail.com>
2023-05-28 21:23:08 +0530

    basic err handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,40 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str>{
+        if args.len() < 3 {
+            return Err("Not Enough Arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config {query, filename})
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
-    let contents = fs::read_to_string(filename).expect("Error reading the file");
+    let config = Config::new(&args).unwrap_or_else(|error| {
+        println!("Problem parsing Arguments: {}", error);
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        println!("Application Error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
     println!("{:?}", contents);
+    Ok(())
 }
